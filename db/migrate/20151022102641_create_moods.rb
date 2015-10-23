2166a91e98ca517654446069c129d19e2f4b27cc
andre <drelucas@gmail.com>
2015-10-22 22:58:58 -0200

    controllers

--- db/migrate/20151022102641_create_moods.rb
+++ db/migrate/20151022102641_create_moods.rb
@@ -1,7 +1,7 @@
 class CreateMoods < ActiveRecord::Migration
   def change
     create_table :moods do |t|
-      t.string :type
+      t.string :kind
       t.string :image_url
 
       t.timestamps null: false
