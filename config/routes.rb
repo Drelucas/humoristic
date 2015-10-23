f7127a466a831582109da0a68aa1f56ce9c12876
andre <drelucas@gmail.com>
2015-10-22 22:45:41 -0200

    views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :users
+  resources :teams
+  resources :moods
+  resources :controls_mood
   get 'home/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
